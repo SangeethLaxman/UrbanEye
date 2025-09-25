733275867d546fa8c41fa9883605bf9576fa5cfe
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-09-25 19:38:50 +0400

    Migration to a React-Based Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './Container.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Container></Container>
+      <p>hi this is very cool</p>
     </>
   )
 }
-
-
 
 export default App
