30f56487068059e4c077b04d780bfcf097b6b770
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-09-25 20:30:33 +0400

    w

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './Container.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>hi this is very cool</p>
+      <Container></Container>
     </>
   )
 }
+
+
 
 export default App
