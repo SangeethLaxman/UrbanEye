f3fb98b5549c28fd1cec4b95fc50819e0b482971
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-10-17 20:59:55 +0400

    rishwanth coolboy

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const {GoogleGenAI} = require("@google/genai")
+const {GoogleGenerativeAI} = require("@google/genai")
 
 export default async function handler(req, res) {
   const allowedOrigins = [
