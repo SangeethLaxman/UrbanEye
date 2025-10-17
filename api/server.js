5ce4ac9281568f125fbad6f8bb97df94e32d36ca
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-10-17 20:48:26 +0400

    tatas

--- api/server.js
+++ api/server.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Failed to generate content' });
+      console.error(err)
     }
   } else {
     res.status(405).json({ error: 'Method not allowed' });
