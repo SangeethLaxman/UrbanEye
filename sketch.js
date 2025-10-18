85b105f5101dc2a9cf0087d584012a5361455c28
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-10-18 10:03:04 +0400

    bodmas

--- sketch.js
+++ sketch.js
@@ -335,8 +335,8 @@ function getResults(results, error) {
     }
 
     imageArray[classificationIndex]["label"] = results[0].label //Modify the label to the recognized label
-    console.log(!imageArray[index]['label'] == "" && imageArray[index]['score'] > 0)
-    howToButton.disabled = !imageArray[index]['label'] == "" && imageArray[index]['score'] > 0
+    console.log(!(imageArray[index]['label'] == "" && imageArray[index]['score'] > 0))
+    howToButton.disabled = !(imageArray[index]['label'] == "" && imageArray[index]['score'] > 0)
     try { 
         imageArray[classificationIndex]["score"] = scoreReference[results[0].label] //Modify the score to the respective points
     } catch (error) {
